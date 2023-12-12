3213a27fda1f9344c3239cc84ce0128cab8eb781
Ewerton Henrique <ewerton.ramiro@gmail.com>
2023-12-12 17:54:10 -0300

    Delete do formulario feita no dia 12/12

--- proc_apagar_usuario.php
+++ proc_apagar_usuario.php
@@ -3,6 +3,7 @@
 session_start();
 include_once("conexao.php");
 $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
+
 $result_usuario = "DELETE FROM usuarios WHERE id='$id'";
 $resultado_usuario = mysqli_query($conn, $result_usuario);
 
